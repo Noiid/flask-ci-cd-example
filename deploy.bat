65568897cd67ca96eb7f929b79e88a18e7395fcc
Luthfi Hariz <luthfihariz@gmail.com>
2024-06-12 20:07:00 +0700

    github action

--- deploy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-docker build -t "blog-app-ci-cd" -f "Dockerfile" .
-
-docker run -p 8080:8080 "blog-app-ci-cd"
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t 'blog-app-ci-cd' -f 'Dockerfile.local' .
-
-docker run -p 8080:8080 'blog-app-ci-cd'
-
