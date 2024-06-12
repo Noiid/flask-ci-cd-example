788eb3977503854a2958d58a89f26dcb7ae55f3e
Luthfi Hariz <luthfihariz@gmail.com>
2024-06-12 17:08:38 +0700

    github check

--- src/blog/routes/post_routes.py
+++ src/blog/routes/post_routes.py
@@ -18,7 +18,7 @@ def create_post():
     except ValueError as e:
         return {"error": str(e)}, 400
 
-    return post, 201
+    return {}, 200
 
 
 @post_routes.route("/posts/<string:id>", methods=["GET"])
